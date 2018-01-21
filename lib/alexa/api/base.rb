8c106a72bf1faaf44de9048e43cc4829b8fdd351
Mike O'Sullivan <mike.osullivan@gmail.com>
2018-01-21 17:15:04 -0500

    error on libxml

--- lib/alexa/api/base.rb
+++ lib/alexa/api/base.rb
@@ -6,7 +6,6 @@ module Alexa
       attr_reader :arguments, :response_body
 
       def initialize(credentials)
-        MultiXml.parser = :libxml
         if MultiXml.parser.to_s == "MultiXml::Parsers::Ox"
           raise StandardError, "MultiXml parser is set to :ox - alexa gem will not work with it currently, use one of: :libxml, :nokogiri, :rexml"
         end
